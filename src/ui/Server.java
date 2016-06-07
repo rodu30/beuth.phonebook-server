ba3e0dd73e1e85ad440a1354d360bc2a7db63cac
Roman <roman.duhr@gmail.com>
2016-06-07 18:41:28 +0200

    Server weiter gemacht

--- src/ui/Server.java
+++ src/ui/Server.java
@@ -1,11 +1,18 @@
 package ui;
 
 import dataStructure.EntryPair;
+import search.NameSearch;
+import search.NumberSearch;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 
 /**
  * Created by romanduhr on 07.06.16.
@@ -50,31 +57,90 @@ public class Server {
             // read input
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String line = in.readLine();
-            System.out.println("Incoming: " + line);
+            System.out.println("Incoming: '" + line + "'");
 
-            // ignore favicon and send side
+            // ignore favicon
             if (line.startsWith("GET /favicon")) {
                 System.out.println("Favicon request");
                 in.close();
                 continue;           // jump to next request ??
             }
-//            else if (line.contains("?")) {
-//                System.out.println("HTTP request with query data");
-//                String[] get = line.split(" ");
-//                String[] l = get[1].substring(2).split("&");
-//                for (String str : l) {
-//                    String[] k = str.split("=");
-//                    if (k.length > 1) {
-//                        map.put(k[0], URLDecoder.decode(k[1], "UTF-8"));
-//                    } else {
-//                        map.put(k[0], "");
-//                    }
-//                }
-//            }
+
+            // process request and send result
+            if (line.contains("?")) {
+                System.out.println("HTTP request with query data");
+                // get name & number
+                String[] query = line.split("=");
+                String[] s1 = query[1].split("&");
+                String inpName = URLDecoder.decode(s1[0], "UTF-8");
+                String[] s2 = query[2].split("&");
+                String inpNumber = URLDecoder.decode(s2[0], "UTF-8");
+                String quit = URLDecoder.decode(query[3], "UTF-8");
+                // start search
+                if (inpName != "" && inpNumber == "") {
+                    if (isValid(inpName)) {
+                        System.out.println("looking for " + inpName);
+                        ArrayList<String> result = new ArrayList<>();
+                        Thread t1 = new Thread(new NameSearch(phonebook, inpName, result));
+                        t1.start();
+                        try {
+                            t1.join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        // TODO: zu HTML ändern
+                        printResult(inpName, result);
+                    } else {
+                        // TODO: zu HTML ändern
+                        printError();
+                    }
+                } else if (inpName == "" && inpNumber != "") {
+                    if (isValid(inpNumber)) {
+                        System.out.println("looking for " + inpNumber);
+                        ArrayList<String> result = new ArrayList<>();
+                        Thread t1 = new Thread(new NumberSearch(phonebook, inpNumber, result));
+                        t1.start();
+                        try {
+                            t1.join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        // TODO: zu HTML ändern
+                        printResult(inpNumber, result);
+                    } else {
+                        // TODO: zu HTML ändern
+                        printError();
+                    }
+                } else if (inpName != "" && inpNumber != "") {
+                    if (isValid(inpName + inpNumber)) {
+                        System.out.println("looking for " + inpName + " & " + inpNumber);
+                        ArrayList<String> result = new ArrayList<>();
+                        Thread t3 = new Thread(new NameSearch(phonebook, inpName, result));
+                        Thread t5 = new Thread(new NumberSearch(phonebook, inpNumber, result));
+                        t3.start();
+                        t5.start();
+                        try {
+                            t3.join();
+                            t5.join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        // TODO: zu HTML ändern
+                        printResult(inpName + inpNumber, result);
+                    } else {
+                        // TODO: zu HTML ändern
+                        printError();
+                    }
+                } else if (quit == "Quit server") {
+                    printQuit();
+                    System.exit(0);
+                } else {
+                    // TODO: zu HTML ändern
+                    printError();
+                }
+            }
 
 
-
-//            {
                 System.out.println("Request processed");
 //                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
@@ -97,7 +163,8 @@ public class Server {
                 out.println("Please enter a number:<br>");
                 out.println("<input type=\"text\" name=\"number\" value=\"123\">");
                 out.println("<br>");
-                out.println("<input type=\"submit\">");
+                out.println("<input type=\"submit\" value=\"Search\">");
+                out.println("<input type=\"submit\" name=\"quitserver\" value=\"Quit server\">");
                 out.println("</form>");
                 out.println("</body>");
                 out.println("</html>");
@@ -106,13 +173,49 @@ public class Server {
                 out.close();
                 in.close();
 
-//            }
-
         }
     }
 
+    private void printQuit() {
+    }
 
 
+    /**
+     * checks if given result list is empty and print results
+     *
+     * @param input
+     * @param result
+     */
+    private void printResult(String input, ArrayList<String> result) {
+        if (result.isEmpty()) {
+            System.out.println("Search for " + input + " not successful. Please try again.");
+        } else {
+            for (String e : result) {
+                System.out.println(e);
+            }
+        }
+    }
 
+    /**
+     * checks if input is valid (no empty input, no whitespaces, no tabs)
+     *
+     * @param input
+     * @return boolean
+     */
+    private boolean isValid(String input) {
+        if (input.isEmpty() || input.matches("\\s+") || input.matches("\\t+")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * prints error message
+     */
+    private void printError() {
+        System.out.println("Not a valid input, please try again.");
+    }
 
 }
+
